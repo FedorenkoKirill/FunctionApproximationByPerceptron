097e4c59367c5819da4075f3cdd51f69d9711233
TK_User <tk_user@Mac-mini-TK-Admin.local>
2021-07-01 12:18:19 +0300

    sine wave approximation

--- main.py
+++ main.py
@@ -3,18 +3,23 @@ import numpy as np
 # создаём модель нейросети, используя Keras
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.optimizers import SGD
 
 
 # задаём линейную функцию, которую попробуем приблизить нашей нейронной сетью
 def f(x):
-    return 2 * x + 5
+    return 2 * np.sin(x) + 5
+
 
 def baseline_model():
     model = Sequential()
-    model.add(Dense(1, input_dim=1, activation='linear'))
-    model.compile(loss='mean_squared_error', optimizer='sgd')
+    # Hidden - Layers
+    model.add(Dense(100, input_dim=1, activation='relu'))
+    # Output- Layer
+    model.add(Dense(1, input_dim=100, activation='linear'))
+    sgd = SGD(lr=0.01, momentum=0.9, nesterov=True)
+    model.compile(loss='mean_squared_error', optimizer=sgd)
     return model
-
 
 if __name__ == "__main__":
     # накидываем тысячу точек от -3 до 3
@@ -25,7 +30,7 @@ if __name__ == "__main__":
 
     # тренируем сеть
     model = baseline_model()
-    model.fit(x, y, nb_epoch=100, verbose=0)
+    model.fit(x, y, epochs=100, verbose=0)
 
     # отрисовываем результат приближения нейросетью поверх исходной функции
     plt.scatter(x, y, color='black', antialiased=True)
